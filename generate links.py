9b94f2d91795affde32346d7f19ae0dc6350b022
Braden <bmw02002turbo@gmail.com>
2021-01-09 15:45:20 -0800

    General Cleanup to generate_links()

--- generate links.py
+++ generate links.py
@@ -1,18 +1,8 @@
 from bs4 import BeautifulSoup, SoupStrainer
 from urllib.request import urlopen
-import re
-import ssl
-
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
 
 
 def generate_links(url):
-    website = urlopen(url, context=ctx).read()
-    links = BeautifulSoup(website, parse_only=SoupStrainer('a'))
+    website_html = urlopen(url)
+    links = BeautifulSoup(website_html, parse_only=SoupStrainer('a'))
     print([link['href'] for link in links if link.has_attr('href')])
-
-
-generate_links("https://www.wikipedia.org/")
