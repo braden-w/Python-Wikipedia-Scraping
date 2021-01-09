33444b14e01b88d9b562a2edb5e3dd13722d6bf3
Braden <bmw02002turbo@gmail.com>
2021-01-09 15:47:23 -0800

    Add SoupStrainer

--- generate links.py
+++ generate links.py
@@ -6,3 +6,6 @@ def generate_links(url):
     website_html = urlopen(url)
     links = BeautifulSoup(website_html, parse_only=SoupStrainer('a'))
     print([link['href'] for link in links if link.has_attr('href')])
+
+
+generate_links("https://www.wikipedia.com/en/Adolf_Hitler")
