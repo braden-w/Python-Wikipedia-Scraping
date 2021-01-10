c959d6aa2840a1b314dc804abb30259aef385561
Braden <bmw02002turbo@gmail.com>
2021-01-10 09:44:48 -0800

    Modified generate_edges_by_one_step inputs

--- functions.py
+++ functions.py
@@ -11,11 +11,10 @@ class Graph(nx.Graph):
     # def from_a_to_b(a, b, path=[]):
     #     self.G.add_nodes(generate_edges_by_one_step(a))
     #         from_a_to_b(generate_edges_by_one_step(key), b, path + list(key))
-    def generate_edges_by_one_step(page_name, language="en"):
+    def generate_edges_by_one_step(
+        self,
+    ):
         # page_name is a page name, not a URL
-
-        self.wiki_wiki = wikipediaapi.Wikipedia(language)
-        page = wiki_wiki.page(page_name)
         links = page.links
         print(links.keys())
         return links.keys()
