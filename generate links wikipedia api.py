5b9629d7b698a5db2f0835f01e300529c0308a4e
Braden <bmw02002turbo@gmail.com>
2021-01-09 18:26:57 -0800

    Added comments

--- generate links wikipedia api.py
+++ generate links wikipedia api.py
@@ -1,6 +1,8 @@
 import wikipediaapi
 
 
+# page_name is a page name, not a URL
+
 def generate_links(page_name, language = 'en'):
     wiki_wiki = wikipediaapi.Wikipedia(language)
     page = wiki_wiki.page(page_name)
