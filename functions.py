dfa8b083fd268d0832520622340dc0b48e783fa8
Braden <bmw02002turbo@gmail.com>
2021-01-10 15:36:08 -0800

    Working selection, just very slow

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
 import wikipediaapi
-
+import cProfile as profile
 from collections import deque
 import networkx as nx
 
@@ -12,33 +12,33 @@ class Graph(nx.Graph):
         self.start_page = start_page
         self.end_page = end_page
         self.current_parent = start_page
-        self.queue = deque([(start_page, start_page)])
-        self.recurse_until_path()
+        self.queue = deque([self.start_page])
+        profile.runctx("self.recurse_until_path()", globals(), locals())
+        # self.recurse_until_path()
 
     def recurse_until_path(self):
-        while self.queue[0][0] != self.end_page:
+        while self.queue[0] != self.end_page:
             self.generate_node_with_children(self.queue.popleft())
             # self.view_nodes_edges()
         self.generate_node_with_children(self.queue.popleft())
         print(nx.shortest_path(self, source=self.start_page, target=self.end_page))
-        # nx.draw(self, with_labels=True)
-        # plt.show()
         return nx.shortest_path(self, source=self.start_page, target=self.end_page)
 
     def generate_node_with_children(self, node):
-        children = [
-            (node_child, node[0])
-            for node_child in self.wikipedia.page(node).links.keys()
-        ]
-        # print(children)
-        self.add_node(node[0])
-        self.add_edge(node[1], node[0])
-        self.queue.extend(children)
+        try:
+            self.current_parent = node["change_current_parent_to"]
+        except:
+            children = self.wikipedia.page(node).links.keys()
+            # print(children)
+            self.add_node(node)
+            self.add_edge(self.current_parent, node)
+            self.queue.append({"change_current_parent_to": node})
+            self.queue.extend(children)
         # print(self.queue)
 
     def view_nodes_edges(self):
-        # print(list(self.nodes), list(self.edges))
-        print(self.nodes.data())
+        print(list(self.nodes), list(self.edges))
+        # print(self.nodes.data())
 
 
-G = Graph("Germany", "Abkhazia")
+G = Graph("Feyerabend", "11th Infantry Division (Wehrmacht)")
