525daa75b86a248ab79c95dc0fd311dc0ba2793a
Braden <bmw02002turbo@gmail.com>
2021-01-10 13:03:28 -0800

    Working application of Graph!

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import wikipediaapi
+
 from collections import deque
 import networkx as nx
 
@@ -10,26 +11,34 @@ class Graph(nx.Graph):
         self.wikipedia = wikipediaapi.Wikipedia(language)
         self.start_page = start_page
         self.end_page = end_page
-        self.queue = deque()
-        self.queue.append(start_page)
+        self.current_parent = start_page
+        self.queue = deque([(start_page, start_page)])
         self.recurse_until_path()
 
     def recurse_until_path(self):
-        while self.queue[0] != end_page:
+        while self.queue[0][0] != self.end_page:
             self.generate_node_with_children(self.queue.popleft())
-        self.generate_node_with_children(self.queue.popleft()) 
-       return nx.shortest_path(self, source=self.start_page, target=self.end_page)
+            # self.view_nodes_edges()
+        self.generate_node_with_children(self.queue.popleft())
+        print(nx.shortest_path(self, source=self.start_page, target=self.end_page))
+        # nx.draw(self, with_labels=True)
+        # plt.show()
+        return nx.shortest_path(self, source=self.start_page, target=self.end_page)
 
     def generate_node_with_children(self, node):
-        children = self.wikipedia.page(node).links.keys()
-        self.add_node(node)
+        children = [
+            (node_child, node[0])
+            for node_child in self.wikipedia.page(node).links.keys()
+        ]
+        # print(children)
+        self.add_node(node[0])
+        self.add_edge(node[1], node[0])
         self.queue.extend(children)
+        # print(self.queue)
 
     def view_nodes_edges(self):
-        print(list(self.nodes), list(self.edges))
+        # print(list(self.nodes), list(self.edges))
         print(self.nodes.data())
 
 
-G = Graph("Germany", "Spain")
-G.view_nodes_edges()
-nx.draw(G, with_labels=True, with_weight=True)
+G = Graph("Germany", "Abkhazia")
