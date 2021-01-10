1492460e495255b7b9868a09662cf128b049e163
Braden <bmw02002turbo@gmail.com>
2021-01-09 19:04:44 -0800

    More minor changes to functions

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ def generate_links(page_name, language="en"):
     wiki_wiki = wikipediaapi.Wikipedia(language)
     page = wiki_wiki.page(page_name)
     links = page.links
-    print(links)
+    print(links.keys())
     return links.keys()
 
 
