e78804dd377e6e0cda761a855f769aecf225ef2b
Braden <bmw02002turbo@gmail.com>
2021-01-09 19:26:31 -0800

    Converted functions to class Graph

--- functions.py
+++ functions.py
@@ -2,22 +2,23 @@ import wikipediaapi
 
 import networkx as nx
 
-G = nx.Graph()
+class Graph:
+    def __init__(self):
+        self.G = nx.Graph()
+    def from_a_to_b(a, b, path=[]):
+        self.G.add_nodes(generate_edges_by_one_step(a))
+            from_a_to_b(generate_edges_by_one_step(key), b, path + list(key))
+    def generate_edges_by_one_step(page_name, language="en"):
+        # page_name is a page name, not a URL
 
-def generate_edges_by_one_step(page_name, language="en"):
-    # page_name is a page name, not a URL
-
-    wiki_wiki = wikipediaapi.Wikipedia(language)
-    page = wiki_wiki.page(page_name)
-    links = page.links
-    print(links.keys())
-    return links.keys()
-
-
-def from_a_to_b(a, b, path=[]):
-    G.add_nodes(generate_links(a))
-            from_a_to_b(generate_links(key), b, path + list(key))
-
+        wiki_wiki = wikipediaapi.Wikipedia(language)
+        page = wiki_wiki.page(page_name)
+        links = page.links
+        print(links.keys())
+        return links.keys()
 
 
-generate_links("Germany")
+
+
+
+generate_edges_by_one_step("Germany")
