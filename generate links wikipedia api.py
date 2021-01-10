bd51f6110c68738a70fdcd9a8f2506bbb34e5037
Braden <bmw02002turbo@gmail.com>
2021-01-09 18:29:37 -0800

    Formatting

--- generate links wikipedia api.py
+++ generate links wikipedia api.py
@@ -1,9 +1,9 @@
 import wikipediaapi
 
-
 # page_name is a page name, not a URL
 
-def generate_links(page_name, language = 'en'):
+
+def generate_links(page_name, language='en'):
     wiki_wiki = wikipediaapi.Wikipedia(language)
     page = wiki_wiki.page(page_name)
     links = page.links
