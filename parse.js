7cd4154b7583cd4ea65d5c3a1fa5394ea2f30d05
Braden <bmw02002turbo@gmail.com>
2021-01-12 14:03:09 -0800

    Added parse.js demo from MediaWiki API

---
+++ parse.js
@@ -0,0 +1,26 @@
+/**
+ * parse.js
+ *
+ * MediaWiki API Demos
+ * Demo of `Parse` module: Parse content of a page
+ *
+ * MIT License
+ */
+ 
+const url = "https://en.wikipedia.org/w/api.php?" +
+    new URLSearchParams({
+        origin: "*",
+        action: "parse",
+        page: "Pet door",
+        format: "json",
+    	contentmodel: "wikitext"
+    });
+
+try {
+    const req = await fetch(url);
+    const json = await req.json();
+    console.log(json.parse.text["*"]);
+} catch (e) {
+    console.error(e);
+}
+
