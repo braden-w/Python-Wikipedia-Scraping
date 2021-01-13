3152a62ae7085d8883861811488dce7094b6add4
Braden <bmw02002turbo@gmail.com>
2021-01-13 10:49:46 -0800

    Implemented while loop

--- parse.js
+++ parse.js
@@ -1,22 +1,24 @@
 const fetch = require('node-fetch')
 
 const getLinks = async (url = '', requestAsJSON = {}) => {
-	let links = []
-	// while (plcontinue) {
-	requestAsURL = new URLSearchParams(requestAsJSON).toString()
-	const response = await fetch(url + requestAsURL)
-	const json = await response.json()
-	const { continue: { plcontinue }, query: { pages } } = json
+    let links = [];
+    let plcontinue = true;
+    let query, json = {};
+	while (plcontinue) {
+		requestAsURL = new URLSearchParams(requestAsJSON).toString()
+		const response = await fetch(url + requestAsURL)
+        json = await response.json();
+        ({ continue: { plcontinue }, query: { pages } } = json);
 
-	for (const pageID in pages) {
-		const page = pages[pageID]
-		const pageLinks = page.links
-		for (const linkObject in pageLinks) {
-			links.push(pageLinks[linkObject].title)
+		for (const pageID in pages) {
+			const page = pages[pageID]
+			const pageLinks = page.links
+			for (const linkObject in pageLinks) {
+				links.push(pageLinks[linkObject].title)
+			}
 		}
+		requestAsJSON.plcontinue = plcontinue
 	}
-	requestAsJSON.plcontinue = plcontinue
-	// }
 	return links
 }
 const url = 'https://en.wikipedia.org/w/api.php?'
