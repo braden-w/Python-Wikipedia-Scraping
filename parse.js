49c73a30965f1db3fc366f74d8d9623feb23fff9
Braden <bmw02002turbo@gmail.com>
2021-01-13 10:43:07 -0800

    Implemented JS Destructuring and formatting changes

--- parse.js
+++ parse.js
@@ -1,29 +1,30 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch')
 
 const getLinks = async (url = '', requestAsJSON = {}) => {
-    requestAsURL = new URLSearchParams(requestAsJSON).toString()
-    const response = await fetch(url + requestAsURL);
-    const json = await response.json();
-    const pages = json.query.pages;
-    let links = [];
-    console.log(pages)
-    console.log(json)
-    for (const pageID in pages) {
-        const page = pages[pageID];
-        const pageLinks = page.links;
-        for (const linkObject in pageLinks) {
-            links.push(pageLinks[linkObject].title);
-        }
-    }
-    return links
+	let links = []
+	// while (plcontinue) {
+	requestAsURL = new URLSearchParams(requestAsJSON).toString()
+	const response = await fetch(url + requestAsURL)
+	const json = await response.json()
+	const { continue: { plcontinue }, query: { pages } } = json
+
+	for (const pageID in pages) {
+		const page = pages[pageID]
+		const pageLinks = page.links
+		for (const linkObject in pageLinks) {
+			links.push(pageLinks[linkObject].title)
+		}
+	}
+	requestAsJSON.plcontinue = plcontinue
+	// }
+	return links
+}
+const url = 'https://en.wikipedia.org/w/api.php?'
+const data = {
+	action: 'query',
+	format: 'json',
+	prop: 'links',
+	titles: 'Hello'
 }
 
-const url = "https://en.wikipedia.org/w/api.php?"
-const data = {
-        action: "query",
-        format: "json",
-        prop: "links",
-        titles:"Hello"
-    };
-
-getLinks(url, data).then(links => console.log(links))
+getLinks(url, data).then((links) => console.log(links))
