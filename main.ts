0d869d1b5012f848cdc5a46493b4d3d33600b4a1
Braden <bmw02002turbo@gmail.com>
2021-01-13 18:21:35 -0800

    Added performance timer

--- main.ts
+++ main.ts
@@ -47,4 +47,8 @@ const getLinks = async (
 	return links;
 }
 // console.log(await getLinks("Hello"))
+var t0 = performance.now()
+
 console.log(await startWeb("Feyerabend", "Germany"))
+var t1 = performance.now()
+console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
