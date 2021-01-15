43614662c092293e34318ec910024fa1fe10d158
Braden <bmw02002turbo@gmail.com>
2021-01-15 14:53:48 -0800

    Added optional test using .then(). Much faster than await

--- main.ts
+++ main.ts
@@ -48,6 +48,6 @@ const getLinks = async (
 // console.log(await getLinks("Hello"))
 var t0 = performance.now()
 
-console.log(await startWeb("Feyerabend", "Germany"))
+// getLinks("Feyerabend").then(links => console.log(links));
 var t1 = performance.now()
 console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
